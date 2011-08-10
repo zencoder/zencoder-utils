066cefc72bf4731c26cc2b6e64fa9abe27447306
Justin Greer <justin@zencoder.com>
2011-08-10 10:27:52 -0500

    Fix major slowdown/memory hog, and improve lots, like having two separate axes.

--- load_analysis/worker_load_analysis.rb
+++ load_analysis/worker_load_analysis.rb
@@ -4,34 +4,39 @@ TIME_OFFSET = -5.hours
 CLOUD_ID = 1
 EXCLUDE_LOW_PRIORITY = true
 
-DURATION = 24.hours
+DURATION = 12.hours
 START_TIME = Time.now - DURATION
-# START_TIME = Time.gm(2011,07,20,0,0,0)
+# START_TIME = Time.gm(2011,8,8,21,57,0)
+# START_TIME = Time.gm(2011,8,9,8,4,0)
 DATA_POINTS = 1440
 # DATA_POINTS = 6.hours / 15.seconds
 
-# SETS_TO_SHOW = [:launched_workers, :active_workers, :bad_workers, :launched_worker_input_capacity, :active_worker_input_capacity, :bad_worker_input_capacity, :launched_worker_output_capacity, :active_worker_output_capacity, :bad_worker_output_capacity, :queued_inputs, :processing_inputs, :queued_outputs, :queued_output_load, :processing_outputs, :processing_output_load]
-# SETS_TO_SHOW = [:launched_workers, :active_workers, :queued_inputs, :processing_inputs, :queued_output_load, :processing_output_load, :active_worker_output_capacity]
-SETS_TO_SHOW = [:launched_workers, :active_worker_output_capacity, :queued_output_load, :processing_output_load]
+all_data_sets = [:launched_workers, :active_workers, :bad_workers, :launched_worker_input_capacity, :active_worker_input_capacity, :bad_worker_input_capacity, :launched_worker_output_capacity, :active_worker_output_capacity, :bad_worker_output_capacity, :queued_inputs, :processing_inputs, :queued_outputs, :queued_output_load, :processing_outputs, :processing_output_load, :input_autoscale_threshold, :output_autoscale_threshold]
+output_scaling_data_sets = [:launched_workers, :active_workers, :launched_worker_output_capacity, :output_autoscale_threshold, :active_worker_output_capacity, :queued_output_load, :processing_output_load]
+input_scaling_data_sets = [:launched_workers, :active_workers, :launched_worker_input_capacity, :input_autoscale_threshold, :active_worker_input_capacity, :queued_inputs, :processing_inputs]
+
+SETS_TO_SHOW = output_scaling_data_sets
 
 #####################################################################
 
 @sets_config = {
-  :launched_workers                => { :color => '#ccaa00', :desc => 'Launched Workers' },
-  :active_workers                  => { :color => '#9999ff', :desc => 'Active Workers' },
-  :bad_workers                     => { :color => '#663399', :desc => 'Bad Workers' },
-  :launched_worker_input_capacity  => { :color => '#339933', :desc => 'Launched Worker Input Capacity' },
-  :active_worker_input_capacity    => { :color => '#99ff66', :desc => 'Active Worker Input Capacity' },
-  :bad_worker_input_capacity       => { :color => '#669900', :desc => 'Bad Worker Input Capacity' },
-  :launched_worker_output_capacity => { :color => '#339999', :desc => 'Launched Worker Output Capacity' },
-  :active_worker_output_capacity   => { :color => '#33ff33', :desc => 'Active Worker Output Capacity' },
-  :bad_worker_output_capacity      => { :color => '#66ffff', :desc => 'Bad Worker Output Capacity' },
-  :queued_inputs                   => { :color => '#993333', :desc => 'Queued Inputs' },
-  :processing_inputs               => { :color => '#ff6699', :desc => 'Processing Inputs' },
-  :queued_outputs                  => { :color => '#339966', :desc => 'Queued Outputs' },
-  :queued_output_load              => { :color => '#6600cc', :desc => 'Queued Output Load' },
-  :processing_outputs              => { :color => '#99ffcc', :desc => 'Processing Outputs' },
-  :processing_output_load          => { :color => '#cc0066', :desc => 'Processing Output Load' },
+  :launched_workers                => { :color => '#ccaa00', :capacity_scale => false, :desc => 'Launched Workers' },
+  :active_workers                  => { :color => '#9999ff', :capacity_scale => false, :desc => 'Active Workers' },
+  :bad_workers                     => { :color => '#663399', :capacity_scale => false, :desc => 'Bad Workers' },
+  :launched_worker_input_capacity  => { :color => '#339933', :capacity_scale => false, :desc => 'Launched Worker Input Capacity' },
+  :active_worker_input_capacity    => { :color => '#99ff66', :capacity_scale => false, :desc => 'Active Worker Input Capacity' },
+  :bad_worker_input_capacity       => { :color => '#669900', :capacity_scale => false, :desc => 'Bad Worker Input Capacity' },
+  :launched_worker_output_capacity => { :color => '#339999', :capacity_scale => true,  :desc => 'Launched Worker Output Capacity' },
+  :active_worker_output_capacity   => { :color => '#33ff33', :capacity_scale => true,  :desc => 'Active Worker Output Capacity' },
+  :bad_worker_output_capacity      => { :color => '#66ffff', :capacity_scale => true,  :desc => 'Bad Worker Output Capacity' },
+  :queued_inputs                   => { :color => '#993333', :capacity_scale => false, :desc => 'Queued Inputs' },
+  :processing_inputs               => { :color => '#ff6699', :capacity_scale => false, :desc => 'Processing Inputs' },
+  :queued_outputs                  => { :color => '#339966', :capacity_scale => false, :desc => 'Queued Outputs' },
+  :queued_output_load              => { :color => '#6600cc', :capacity_scale => true,  :desc => 'Queued Output Load' },
+  :processing_outputs              => { :color => '#99ffcc', :capacity_scale => false, :desc => 'Processing Outputs' },
+  :processing_output_load          => { :color => '#cc0066', :capacity_scale => true,  :desc => 'Processing Output Load' },
+  :input_autoscale_threshold       => { :color => '#77aa44', :capacity_scale => false, :desc => 'Input Autoscale Threshold' },
+  :output_autoscale_threshold      => { :color => '#338033', :capacity_scale => true,  :desc => 'Output Autoscale Threshold' },
 }
 
 
@@ -41,9 +46,13 @@ SETS_TO_SHOW = [:launched_workers, :active_worker_output_capacity, :queued_outpu
 @cloud = Cloud.find(CLOUD_ID)
 @cloud.worker_types.all.each { |wt| @worker_types[wt.id] = wt }
 
+puts "Querying database for data..."
+
 @workers = @cloud.workers.find(:all, :select => 'id,worker_type_id,state,created_at,alive_at,killed_at,updated_at,url,instance_id', :conditions => ["(killed_at >= ? OR (killed_at is null AND updated_at >= ?)) AND created_at < ?", @start_time, @start_time, @end_time])
 @inputs = @cloud.input_media_files.find(:all, :select => 'id,account_id,state,created_at,started_at,finished_at,times,low_priority', :conditions => ["(finished_at is null or finished_at >= ?) and created_at < ? and state != 'cancelled'", @start_time, @end_time])
 @outputs = @cloud.output_media_files.find(:all, :select => 'id,account_id,job_id,state,created_at,started_at,finished_at,times,low_priority,cached_queue_time,cached_total_time,estimated_transcode_load', :conditions => ["(finished_at is null or finished_at >= ?) and created_at < ? and state != 'cancelled'", @start_time, @end_time])
+
+puts "Queries finished."
 
 
 @worker_time_data = []
@@ -56,20 +65,30 @@ SETS_TO_SHOW = [:launched_workers, :active_worker_output_capacity, :queued_outpu
   input_capacity = @worker_types[worker.worker_type_id].max_downloads || 6
   output_capacity = @worker_types[worker.worker_type_id].max_load || 900
 
+  created = worker.created_at.to_i
+  updated = worker.updated_at.to_i
+  if worker.alive_at && (worker.alive_at - worker.created_at > 30.minutes)
+    alive = created + 4.minutes
+  else
+    alive = (worker.alive_at || Time.now + 1.year).to_i
+  end
+  killed = (worker.killed_at || Time.now + 1.year).to_i
+
   if worker.url.blank? && ['terminated','disappeared'].include?(worker.state)
-    @worker_time_data << { :type => :bad_launch, :time => worker.created_at.to_i, :input_capacity => input_capacity, :output_capacity => output_capacity }
-    @worker_time_data << { :type => :bad_kill, :time => worker.updated_at.to_i, :input_capacity => input_capacity, :output_capacity => output_capacity }
+    @worker_time_data << { :type => :bad_launch, :time => created, :input_capacity => input_capacity, :output_capacity => output_capacity }
+    @worker_time_data << { :type => :bad_kill,   :time => updated, :input_capacity => input_capacity, :output_capacity => output_capacity }
   elsif worker.url.present? || ['launching'].include?(worker.state)
     # Good worker.
-    @worker_time_data << { :type => :launched, :time => worker.created_at.to_i, :input_capacity => input_capacity, :output_capacity => output_capacity }
-    @worker_time_data << { :type => :active, :time => (worker.alive_at || Time.now + 1.year).to_i, :input_capacity => input_capacity, :output_capacity => output_capacity }
-    @worker_time_data << { :type => :killed, :time => (worker.killed_at || Time.now + 1.year).to_i, :input_capacity => input_capacity, :output_capacity => output_capacity }
+    @worker_time_data << { :type => :launched, :time => created, :input_capacity => input_capacity, :output_capacity => output_capacity }
+    @worker_time_data << { :type => :active,   :time => alive,   :input_capacity => input_capacity, :output_capacity => output_capacity }
+    @worker_time_data << { :type => :killed,   :time => killed,  :input_capacity => input_capacity, :output_capacity => output_capacity }
   else
     puts "Ignoring bad worker record - #{worker.id}"
   end
 
 end
 @worker_time_data = @worker_time_data.sort_by { |d| d[:time] }
+puts "Loaded worker data..."
 
 # @input_total_times = {}
 @input_time_data = []
@@ -84,6 +103,7 @@ end
   # @input_total_times[input.id] = input.total_time rescue nil
 end
 @input_time_data = @input_time_data.sort_by { |d| d[:time] }
+puts "Loaded input data..."
 
 @output_time_data = []
 @outputs.each do |output|
@@ -111,6 +131,7 @@ end
   @output_time_data << { :type => :finished, :time => output.finished_at.to_i, :load => load }
 end
 @output_time_data = @output_time_data.sort_by { |d| d[:time] }
+puts "Loaded output data..."
 
 puts "Stats Collected: #{@worker_time_data.length} worker entries, #{@input_time_data.length} input entries, #{@output_time_data.length} output entries"
 
@@ -151,6 +172,7 @@ END_HTML
   
   @granularity = (@end_time - @start_time) / DATA_POINTS.to_f
 
+  puts "Analyzing..."
   ############# SET UP WORKER DATA ##############
   prev_point = 0
   @worker_time_data.each do |data|
@@ -163,6 +185,8 @@ END_HTML
         @baselines[:launched_workers] += 1
         @baselines[:launched_worker_input_capacity] += data[:input_capacity]
         @baselines[:launched_worker_output_capacity] += data[:output_capacity]
+        @baselines[:input_autoscale_threshold] += data[:input_capacity] * 1.2
+        @baselines[:output_autoscale_threshold] += data[:output_capacity] * 1.2
       when :active
         @baselines[:active_workers] += 1
         @baselines[:active_worker_input_capacity] += data[:input_capacity]
@@ -183,21 +207,28 @@ END_HTML
 
     offset = data[:time] - @start_time.to_i
     point_loc = (offset / @granularity).round
-    
+
+    point_loc = DATA_POINTS + 1 if point_loc > DATA_POINTS
+
     while prev_point < point_loc
       [:launched_workers, :launched_worker_input_capacity, :launched_worker_output_capacity,
        :active_workers, :active_worker_input_capacity, :active_worker_output_capacity,
-       :bad_workers, :bad_worker_input_capacity, :bad_worker_output_capacity].each do |data_set|
+       :bad_workers, :bad_worker_input_capacity, :bad_worker_output_capacity,
+       :input_autoscale_threshold, :output_autoscale_threshold].each do |data_set|
         @data_points[data_set][prev_point + 1] = @data_points[data_set][prev_point]
       end
       prev_point += 1
     end
-    
+
+    next if point_loc > DATA_POINTS
+
     case data_type
     when :launched
       @data_points[:launched_workers][point_loc] += 1
       @data_points[:launched_worker_input_capacity][point_loc] += data[:input_capacity]
       @data_points[:launched_worker_output_capacity][point_loc] += data[:output_capacity]
+      @data_points[:input_autoscale_threshold][point_loc] += data[:input_capacity] * 1.2
+      @data_points[:output_autoscale_threshold][point_loc] += data[:output_capacity] * 1.2
     when :active
       @data_points[:active_workers][point_loc] += 1
       @data_points[:active_worker_input_capacity][point_loc] += data[:input_capacity]
@@ -206,6 +237,8 @@ END_HTML
       @data_points[:launched_workers][point_loc] -= 1
       @data_points[:launched_worker_input_capacity][point_loc] -= data[:input_capacity]
       @data_points[:launched_worker_output_capacity][point_loc] -= data[:output_capacity]
+      @data_points[:input_autoscale_threshold][point_loc] -= data[:input_capacity] * 1.2
+      @data_points[:output_autoscale_threshold][point_loc] -= data[:output_capacity] * 1.2
       @data_points[:active_workers][point_loc] -= 1
       @data_points[:active_worker_input_capacity][point_loc] -= data[:input_capacity]
       @data_points[:active_worker_output_capacity][point_loc] -= data[:output_capacity]
@@ -219,6 +252,7 @@ END_HTML
       @data_points[:bad_worker_output_capacity][point_loc] -= data[:output_capacity]
     end
   end
+  puts "  Worker analysis done."
 
   ############# SET UP INPUTS DATA ##############
   prev_point = 0
@@ -239,7 +273,9 @@ END_HTML
 
     offset = data[:time] - @start_time.to_i
     point_loc = (offset / @granularity).round
-    
+
+    point_loc = DATA_POINTS + 1 if point_loc > DATA_POINTS
+
     while prev_point < point_loc
       [:queued_inputs, :processing_inputs].each do |data_set|
         @data_points[data_set][prev_point + 1] = @data_points[data_set][prev_point]
@@ -247,6 +283,8 @@ END_HTML
       prev_point += 1
     end
     
+    next if point_loc > DATA_POINTS
+
     case data_type
     when :queued
       @data_points[:queued_inputs][point_loc] += 1
@@ -257,6 +295,7 @@ END_HTML
       @data_points[:processing_inputs][point_loc] -= 1
     end
   end
+  puts "  Input analysis done."
 
   ############# SET UP OUPUTS DATA ##############
   prev_point = 0
@@ -279,7 +318,9 @@ END_HTML
 
     offset = data[:time] - @start_time.to_i
     point_loc = (offset / @granularity).round
-    
+
+    point_loc = DATA_POINTS + 1 if point_loc > DATA_POINTS
+
     while prev_point < point_loc
       [:queued_outputs, :processing_outputs, :queued_output_load, :processing_output_load].each do |data_set|
         @data_points[data_set][prev_point + 1] = @data_points[data_set][prev_point]
@@ -287,6 +328,8 @@ END_HTML
       prev_point += 1
     end
     
+    next if point_loc > DATA_POINTS
+
     case data_type
     when :queued
       @data_points[:queued_outputs][point_loc] += 1
@@ -301,46 +344,64 @@ END_HTML
       @data_points[:processing_output_load][point_loc] -= data[:load]
     end
   end
+  puts "  Output analysis done."
 
-  ############# SCALE DOWN OUTPUT LOAD ###########
-  max_outputs = [
-    @data_points[:queued_outputs].max + @baselines[:queued_outputs],
-    @data_points[:processing_outputs].max + @baselines[:processing_outputs],
-  ].max
-  max_capacity = [
-    @data_points[:launched_worker_output_capacity].max + @baselines[:launched_worker_output_capacity],
-    @data_points[:active_worker_output_capacity].max + @baselines[:active_worker_output_capacity],
-    @data_points[:bad_worker_output_capacity].max + @baselines[:bad_worker_output_capacity],
-  ].max
-  scale = max_outputs.to_f / max_capacity
-  
-  [:queued_output_load, :launched_worker_output_capacity, :processing_output_load, :active_worker_output_capacity, :bad_worker_output_capacity].each do |data_set|
-    @baselines[data_set] = (@baselines[data_set] * scale).round
-    @data_points[data_set].each_with_index do |value,index|
-      @data_points[data_set][index] = (value * scale).round
-    end
-  end
+  # ############# SCALE DOWN OUTPUT LOAD ###########
+  # max_outputs = [
+  #   @data_points[:queued_outputs].max + @baselines[:queued_outputs],
+  #   @data_points[:processing_outputs].max + @baselines[:processing_outputs],
+  # ].max
+  # max_capacity = [
+  #   @data_points[:launched_worker_output_capacity].max + @baselines[:launched_worker_output_capacity],
+  #   @data_points[:active_worker_output_capacity].max + @baselines[:active_worker_output_capacity],
+  #   @data_points[:bad_worker_output_capacity].max + @baselines[:bad_worker_output_capacity],
+  # ].max
+  # scale = max_outputs.to_f / max_capacity
+  # 
+  # [:queued_output_load, :launched_worker_output_capacity, :processing_output_load, :active_worker_output_capacity, :bad_worker_output_capacity].each do |data_set|
+  #   @baselines[data_set] = (@baselines[data_set] * scale).round
+  #   @data_points[data_set].each_with_index do |value,index|
+  #     @data_points[data_set][index] = (value * scale).round
+  #   end
+  # end
+  # puts "  Scaled results."
 
+  puts "Building output..."
   DATA_POINTS.times do |p|
-    time = (@start_time + (p*@granularity) + TIME_OFFSET).strftime('%H:%M')
+    if DURATION <= 1.hour
+      time = (@start_time + (p*@granularity) + TIME_OFFSET).strftime('%H:%M:%S')
+    else
+      time = (@start_time + (p*@granularity) + TIME_OFFSET).strftime('%H:%M')
+    end
     values = []
     SETS_TO_SHOW.each do |data_set|
       values << @baselines[data_set] + @data_points[data_set][p].to_i
     end
     f.puts "  ['%s', %s]," % [time, values.join(',')]
   end
+  puts "Done."
   
   f.puts "]);"
   f.puts "var chart = new google.visualization.LineChart(document.getElementById('chart_div'));"
 
-  invert_colors = true
-  inverted_color_spec = ", backgroundColor: 'black', gridlineColor: '#444', legendTextStyle: { color: '#ccc' }, hAxis: { baselineColor: '#999', textStyle: { color: '#ccc' }, titleTextStyle: { color: '#ccc' } }, vAxis: { baselineColor: '#999', titleTextStyle: { color: '#ccc' }, textStyle: { color: '#ccc' } }"
   
-  line_colors = "series: ["
-  line_colors << SETS_TO_SHOW.map { |data_set| "{color: '#{@sets_config[data_set][:color]}'}" }.join(',')
-  line_colors << "]"
+  chart_specs = []
+  # Basics
+  chart_specs << "isStacked: false, lineWidth: 1, width: #{DATA_POINTS + 200}, height: 800"
+  # Layout
+  chart_specs << "chartArea: { width: #{DATA_POINTS} }"
+  # Overall colors
+  chart_specs << "backgroundColor: 'black', gridlineColor: '#444', legendTextStyle: { color: '#ccc', fontSize: 10 }, hAxis: { baselineColor: '#999', textStyle: { color: '#ccc' }, titleTextStyle: { color: '#ccc' } }, vAxis: { baselineColor: '#999', titleTextStyle: { color: '#ccc' }, textStyle: { color: '#ccc' } }"
+  # Vertical axes
+  chart_specs << "vAxes:[{title:'Actual Values', minValue:0},{title:'Capacities/Loads', minValue:0}]"
 
-  f.puts "chart.draw(data, { isStacked: false, lineWidth: 1, width: 1440, height: 800, #{line_colors} #{inverted_color_spec if invert_colors} });"
+  # Line colors
+  line_colors = "series: ["
+  line_colors << SETS_TO_SHOW.map { |data_set| "{color: '#{@sets_config[data_set][:color]}', targetAxisIndex: #{@sets_config[data_set][:capacity_scale] ? 1 : 0}}" }.join(',')
+  line_colors << "]"
+  chart_specs << line_colors
+
+  f.puts "chart.draw(data, { #{chart_specs.join(',')} });"
   f.puts "</script>"
 
 f.puts <<END_HTML
