9150531babfe43467caa53f2cf3e520699d8c456
skidder <skidder@brightcove.com>
2013-06-17 16:41:24 -0700

    Added consolidation of input and output stream logs and annotation of significant events in the chart

--- live_job_latency_grapher.rb
+++ live_job_latency_grapher.rb
@@ -123,64 +123,111 @@ def start_html_output
   <script type="text/javascript">'
 
   @output_stream.puts "  google.load('visualization', '1', {packages: ['annotatedtimeline']});"
+  @output_stream.puts "  var g;"
+  @output_stream.puts "  var rtmp_data_set = new Array();"
+  @output_stream.puts "  var event_data_set = new Array();"
+  @output_stream.puts
+  @output_stream.puts "  function drawVisualization() {"
 end
 
-def start_graph(title)
+# Output a data set for 
+def output_graph_data(title,rtmplog)
   @graph_count += 1
   @graph_names[@graph_count] = title
-  @output_stream.puts "  function drawVisualization#{@graph_count}() {"
-  @output_stream.puts "    var data = new google.visualization.DataTable();"
-  @output_stream.puts "    data.addColumn('datetime', 'Time');"
-  # @output_stream.puts "    data.addColumn('number', 'Average Latency');"
-  # @output_stream.puts "    data.addColumn('number', 'Min Latency');"
-  # @output_stream.puts "    data.addColumn('number', 'Max Latency');"
-  @output_stream.puts "    data.addColumn('number', 'Latency');"
-  @output_stream.puts "    data.addColumn('number', 'Percent Uploaded');"
-  @output_stream.puts "    data.addColumn('number', 'Events');"
-  @output_stream.puts "    data.addColumn('string', 'eventtitle');"
-  @output_stream.puts "    data.addRows(["
-end
+  case @graph_count
+  when 1
+    @output_stream.puts "    // Input dataset"
+    series_name = "Input Latency"
+  else
+    @output_stream.puts "    // Output dataset: #{title}"
+    series_name = "Output Latency"
+  end
 
-def finish_graph
+  
+  # EVENT DATA
+  event_timestamp_hash = Hash.new
+  @output_stream.puts "    event_data_set[#{@graph_count}] = ["
+  rtmplog.events.each do |event|
+    # add entry to the timestamp map for lookup when processing the RTMP logs
+    event_timestamp_hash[Time.at(event[0].to_f.round)] = "default"
+
+    @output_stream.puts "{"
+    @output_stream.puts "series: \"#{series_name}\","
+    @output_stream.puts "x: \"%s\"," % Time.at(event[0].to_f.round).strftime("%F %TZ")
+    @output_stream.puts "shortText: \"#{event[1].to_s}\","
+    @output_stream.puts "text: \"#{event[1].to_s}\","
+    @output_stream.puts "},"
+  end
+  @output_stream.puts "    ];"
+  @output_stream.puts
+
+  # RTMP DATA
+  @output_stream.puts "    var rtmp_data_#{@graph_count} = new google.visualization.DataTable();"
+  @output_stream.puts "    rtmp_data_#{@graph_count}.addColumn('datetime', 'Time');"
+  @output_stream.puts "    rtmp_data_#{@graph_count}.addColumn('number', '#{series_name}');"
+  @output_stream.puts "    rtmp_data_#{@graph_count}.addRows(["
+
+  assumed_stream_baseline = rtmplog.stream_start_time - [rtmplog.stream_start_delay, rtmplog.max_start_offset / 1000.0].max
+  rtmplog.upload_timings.each do |timing|
+    latency = (timing[0] - assumed_stream_baseline)-(timing[2]/1000.0)
+    # puts "TIMING: #{timing[0]} - #{timing[1]} - #{timing[2]} (#{sprintf('%0.3f', latency)})"
+    # case timing[3]
+    # when nil
+      # @output_stream.puts "[new Date(%d), %0.3f, undefined]," % [(timing[0].to_f * 1000).round, latency]
+    # else
+    #   @output_stream.puts "[new Date(%d), %0.3f, \"%s\"]," % [(timing[0].to_f * 1000).round, latency, timing[3]]
+    # end
+
+
+     @output_stream.puts "[new Date(%d), %0.3f]," % [(timing[0].to_f * 1000).round, (latency < 0.0 ? 0 : latency)]
+
+     if event_timestamp_hash[Time.at(timing[0].to_f.round)] == "default" then
+       @output_stream.puts "[new Date(%d), %0.3f]," % [timing[0].to_f.round * 1000, (latency < 0.0 ? 0 : latency)]
+       event_timestamp_hash[Time.at(timing[0].to_f.round)] = "complete"
+     end
+
+  end
   @output_stream.puts "    ]);"
-  @output_stream.puts
-  @output_stream.puts "    var g = new Dygraph.GVizChart(document.getElementById('visualization-#{@graph_count}'));"
-  @output_stream.puts "    g.draw(data, {displayAnnotations: true, labelsKMB: true, stackedGraph: false});"
-  @output_stream.puts "  }"
-  @output_stream.puts
-  @output_stream.puts "  google.setOnLoadCallback(drawVisualization#{@graph_count});"
-  @output_stream.puts
+  @output_stream.puts "    rtmp_data_set[#{@graph_count}] = rtmp_data_#{@graph_count};"
 end
 
 def finish_html_output
+
+  @output_stream.puts
+  @output_stream.puts "    g = new Dygraph(document.getElementById(\"visualization\"));"
+  # @output_stream.puts "    g = new google.visualization.AnnotatedTimeLine(document.getElementById(\"visualization\"));"
+  @output_stream.puts "    update_graph(2);"
+  @output_stream.puts "  }"
+  @output_stream.puts
+  @output_stream.puts "  google.setOnLoadCallback(drawVisualization);"
+  @output_stream.puts
+
   @output_stream.puts '  </script>
 </head>
 <body style="font-family: Arial;border: 0 none;">'
   @output_stream.puts "<h2>Job #{@job_id} Live Stream Latency (#{@job_is_live ? 'Currently Streaming' : 'Finished'})</h2>"
+  @output_stream.puts '<div id="visualization" style="width: 100%; height: 400px;"></div>'
+  @output_stream.puts " <p><b>Show Series:</b></p>
+    <p>"
 
-  1.upto(@graph_count) do |i|
-    @output_stream.puts "<h3>#{@graph_names[i]}</h3>"
-    @output_stream.puts '<div id="visualization-' + i.to_s + '" style="width: 100%; height: 400px;"></div>'
+  2.upto(@graph_count) do |i|
+    @output_stream.puts " <input type=\"radio\" name=\"outputs\" id=\"#{i}\" onClick=\"change(this)\" #{i == 2 ? "checked=\"checked\"" : ""}><label for=\"#{i}\">#{@graph_names[i]}</label><br/>"
   end
 
+  @output_stream.puts "<script type=\"text/javascript\">
+      function change(el) {
+        update_graph(el.id);
+      }
+      function update_graph(i) {
+        var merged_data = google.visualization.data.join(rtmp_data_set[1], rtmp_data_set[i], 'full', [[0,0]], [1], [1]);
+        g.updateOptions({ 'file': merged_data, connectSeparatedPoints: true, legend: 'always', axisLabelFontSize: 12, displayAnnotations: true, showRangeSelector: true, valueRange: [0,null] } );
+        g.setAnnotations(event_data_set[1].concat(event_data_set[i]));
+      }
+      </script>"
   @output_stream.puts '</body>
 </html>'
 end
 
-def output_graph_data(rtmplog)
-  assumed_stream_baseline = rtmplog.stream_start_time - [rtmplog.stream_start_delay, rtmplog.max_start_offset / 1000.0].max
-  rtmplog.upload_timings.each do |timing|
-    latency = (timing[0] - assumed_stream_baseline)-(timing[2]/1000.0)
-    # percent_uploaded = (timing[2].to_f / @max_stream_time) * @max_latency
-    percent_uploaded = (timing[2].to_f / rtmplog.max_stream_time) * 100.0
-    # puts "TIMING: #{timing[0]} - #{timing[1]} - #{timing[2]} (#{sprintf('%0.3f', latency)})"
-    @output_stream.puts "[new Date(%d), %0.3f, %0.3f, null, null]," % [(timing[0].to_f * 1000).round, latency, percent_uploaded]
-  end
-  
-  rtmplog.events.each do |event|
-    @output_stream.puts "[new Date(%d), null, null, 0, '%s']," % [(event[0].to_f * 1000).round, event[1].to_s]
-  end
-end
 
 class RTMPLog
   attr_accessor :stream_start_time, :stream_start_delay, :max_start_offset, :upload_timings, :max_stream_time, :events
@@ -219,6 +266,7 @@ class RTMPLog
     @line_count = 0
     @stream_start_delay = 0
     @stream_start_time = nil
+    @last_stream_time = 0
 
 
     while line = logfile.gets
@@ -231,26 +279,37 @@ class RTMPLog
 
       case line
         when /WARNING: Auth failed/
-          @events << [line_timestamp, :auth_failure]
+          @events << [line_timestamp, time, stream_time]
+          @upload_timings << [line_timestamp, stream_start_time, @last_stream_time, nil]
         when /DEBUG: Invoking FCPublish/
           @events << [line_timestamp, :fc_publish]
+          @upload_timings << [line_timestamp, stream_start_time, @last_stream_time, nil]
+
         when /INFO: STREAM_START (\d+)/
           stream_start_time = $1.to_i
+          @last_stream_time = 0
           @stream_start_time = line_timestamp
           @stream_start_delay = (line_timestamp - @first_timestamp_seen)
           @events << [line_timestamp, :started_streaming]
-          @upload_timings << [line_timestamp, stream_start_time, 0]
+          @upload_timings << [line_timestamp, stream_start_time, 0, nil]
+          @last_time = line_timestamp
         when /DEBUG: New start time offset: (\d+)/
           @max_start_offset = $1.to_i
         when /INFO: TIME (\d+) OFFSET (\d+) STREAM_TIME (\d+)/
           time,offset,stream_time = [$1.to_i, $2.to_i, $3.to_i]
-          @upload_timings << [line_timestamp, time, stream_time]
+          @upload_timings << [line_timestamp, time, stream_time, nil]
+          @last_time = time
+          @last_stream_time = stream_time
         when /WARNING: WriteN, RTMP send error/
           @events << [line_timestamp, :send_error]
+          @upload_timings << [line_timestamp, stream_start_time, @last_stream_time, nil]
         when /ERROR: disconnected from remote server/
           @events << [line_timestamp, :disconnect]
+          @upload_timings << [line_timestamp, stream_start_time, @last_stream_time, nil]
         when /INFO: send_data, done sending/
           @events << [line_timestamp, :done]
+          @upload_timings << [line_timestamp, stream_start_time, @last_stream_time, nil]
+
       end
     end
     logfile.close
@@ -304,7 +363,12 @@ end
 job = Job.find_by_id(@job_id)
 raise "Job #{@job_id} not found!" unless job
 
+# locate the input log file within the context of the job input directory
 input_log = get_log(job.input_media_file, 'input', 'download/download.log')
+
+# for each of the transcoding outputs, locate the upload log file within the context of the job output directory
+# the upload log contains the rmtppush logging output showing info about the upload activity
+# from zencoder to the CDN/publish point.
 output_logs = job.output_media_files.map { |o| [o.label, get_log(o, 'output', 'transcode/upload.log')] }
 
 puts "Got input log: #{input_log}"
@@ -323,12 +387,11 @@ all_logs = [['Input', input_log]] + output_logs.sort_by { |ol| ol.first.to_s }
 @output_stream = File.open('live_job_latency_graph.html','w')
 start_html_output
 
+# parse the input stream RTMP log
 all_logs.each do |log_info|
   rtmplog = RTMPLog.new(log_info[1])
   if rtmplog.valid?
-    start_graph(log_info[0])
-    output_graph_data(rtmplog)
-    finish_graph
+    output_graph_data(log_info[0],rtmplog)
   end
 end
 
