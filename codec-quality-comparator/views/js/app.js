7a02be1c35cf7160148f9c73526cc829f4f305b6
Alex Schworer <aschworer@brightcove.com>
2014-07-14 11:52:36 -0700

    separate the presentation code from the data in index.html

--- codec-quality-comparator/views/js/app.js
+++ codec-quality-comparator/views/js/app.js
@@ -1,8 +1,6 @@
-$(function() {
-  var data = [
-    { label: 'Layer 1', values: [ {x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 2} ] },
-    { label: 'Layer 2', values: [ {x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 4} ] }
-  ];
-  var areaChartInstance = $('#area').epoch({ type: 'area', data: data });
-
-});
+var drawChart = function(data) {
+  var chart = $('#area').epoch({
+      type: 'line',
+      data: data
+  });
+}
