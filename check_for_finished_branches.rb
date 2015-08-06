4ae57eb70a47c1dcf1aa8765d4cdc17e6d248702
Justin Greer <justin@zencoder.com>
2015-08-06 15:52:04 -0500

    Add more context to check_for_finished_branches

--- check_for_finished_branches.rb
+++ check_for_finished_branches.rb
@@ -16,19 +16,64 @@ remote_hashes = remote_hashes.split(/\n/).map { |line| line.split(/\s+/).compact
 remote_hashes.each do |hash,branch|
   next unless branch.to_s.include?('refs/heads/feature')
 
+  simple_branch_name = branch.sub('refs/heads/','')
+
   # Check for remote branches containing the hash, and see if origin/master is in the mix.
   merged_to_master = system("git branch --contains #{hash} -r 2>&1 | grep --quiet origin/master")
 
-  puts "Branch #{branch.sub('refs/heads/','')} exists, despite already being merged to master." if merged_to_master
+  if merged_to_master
+    responsible_merge_party = 'An Unknown Entity'
+    responsible_merge_date = 'An Unkonwn Date'
+    responsible_merge_commit = nil
+
+    last_updated_info = `git log -n 1 #{hash}`
+    last_updated_date = "#{$1} #{$2}" if last_updated_info =~ /Date: +\S+\s+(\S+\s+\S+)\s+\S+\s+(\S+)/ # Date:   Thu Jul 24 17:07:54 2014 -0700
+
+    # Try to determine who did it, and when.
+    merge_info = `git log origin/master ^origin/#{simple_branch_name} --ancestry-path --merges | grep -B 5 "Merge branch '#{simple_branch_name}'"`
+
+    responsible_merge_party  = $1 if merge_info =~ /Author: +([^<]+?) *</     # Author: Matthew McClure <mmcclure@brightcove.com>
+    responsible_merge_date   = "#{$1} #{$2}" if merge_info =~ /Date: +\S+\s+(\S+\s+\S+)\s+\S+\s+(\S+)/ # Date:   Thu Jul 24 17:07:54 2014 -0700
+    responsible_merge_commit = $& if merge_info =~ /commit [0-9a-f]+/         # commit 1620f62b6849a911a5d61b514dc93d4654b6eee7
+
+    if responsible_merge_commit
+      puts "Branch #{simple_branch_name} exists, despite #{responsible_merge_party} merging it to master on #{responsible_merge_date} in #{responsible_merge_commit}."
+    else
+      puts "Branch #{simple_branch_name} exists, despite already being merged to master.  (Last Activity: #{last_updated_date})"
+    end
+  end
 end
 
 # Check for finished hotfixes (merged to production)
 remote_hashes.each do |hash,branch|
   next unless branch.to_s.include?('refs/heads/hotfix')
 
-  # Check for remote branches containing the hash, and see if origin/master is in the mix.
+  simple_branch_name = branch.sub('refs/heads/','')
+
+  # Check for remote branches containing the hash, and see if origin/production is in the mix.
   merged_to_production = system("git branch --contains #{hash} -r 2>&1 | grep --quiet origin/production")
-  puts "Branch #{branch.sub('refs/heads/','')} exists, despite already being merged to production." if merged_to_production
+
+  if merged_to_production
+    responsible_merge_party = 'An Unknown Entity'
+    responsible_merge_date = 'An Unkonwn Date'
+    responsible_merge_commit = nil
+
+    last_updated_info = `git log -n 1 #{hash}`
+    last_updated_date = "#{$1} #{$2}" if last_updated_info =~ /Date: +\S+\s+(\S+\s+\S+)\s+\S+\s+(\S+)/ # Date:   Thu Jul 24 17:07:54 2014 -0700
+
+    # Try to determine who did it, and when.
+    merge_info = `git log origin/production ^origin/#{simple_branch_name} --ancestry-path --merges | grep -B 5 "Merge branch '#{simple_branch_name}'"`
+
+    responsible_merge_party  = $1 if merge_info =~ /Author: +([^<]+?) *</     # Author: Matthew McClure <mmcclure@brightcove.com>
+    responsible_merge_date   = "#{$1} #{$2}" if merge_info =~ /Date: +\S+\s+(\S+\s+\S+)\s+\S+\s+(\S+)/ # Date:   Thu Jul 24 17:07:54 2014 -0700
+    responsible_merge_commit = $& if merge_info =~ /commit [0-9a-f]+/         # commit 1620f62b6849a911a5d61b514dc93d4654b6eee7
+
+    if responsible_merge_commit
+      puts "Branch #{simple_branch_name} exists, despite #{responsible_merge_party} merging it to production on #{responsible_merge_date} in #{responsible_merge_commit}."
+    else
+      puts "Branch #{simple_branch_name} exists, despite already being merged to production.  (Last Activity: #{last_updated_date})"
+    end
+  end
 end
 
 
